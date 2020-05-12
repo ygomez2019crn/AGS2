fe1b8573591c1e9e4ca12bda24d1536273400ef9
ygomez2019crn <ygomez.crn@artsgrafiques.org>
2020-05-12 17:14:32 +0200

    Add files via upload

---
+++ test/script.js
@@ -0,0 +1,19 @@
+function funcionfiltro() {
+    var input, filter, ul, li, a, i, txtValue;
+
+    input = document.getElementById("filtro");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("lista");
+    li = ul.getElementsByTagName("span");
+    
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("p")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
